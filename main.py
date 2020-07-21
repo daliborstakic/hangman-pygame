a7124ddb52964b44adab69bb7e646969dabef797
daliborstakic <stakicdalibor64@gmail.com>
2020-07-21 11:23:46 +0200

    Added the render() function.

--- main.py
+++ main.py
@@ -7,21 +7,33 @@ HEIGHT = 600
 surf = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Hangman")
 
+# Images
+hangman_images = []
+for i in range(7):
+    hangman_images.append(pygame.image.load("images\hangman{}.png".format(str(i))))
+
 # Colors
 WHITE = (255, 255, 255)
 
 
+def render(hangman_status):
+    surf.fill(WHITE)
+    surf.blit(hangman_images[hangman_status], (70, (HEIGHT - hangman_images[hangman_status].get_height()) / 2 - 80))
+    pygame.display.update()
+
+
 def main():
+    # Hangman variables
+    hangman_status = 0
+
     run = True
 
     while run:
-        surf.fill(WHITE)
-
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
 
-        pygame.display.update()
+        render(hangman_status)
 
     pygame.quit()
 
